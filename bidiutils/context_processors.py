4942cbc6982c23a6468a77f429390d5accd34b27
Meir Kriheli <meir@mksoft.co.il>
2009-02-03 00:19:21 +0200

    Added bidi context processor

---
+++ bidiutils/context_processors.py
@@ -0,0 +1,29 @@
+"""Context processors for BiDi"""
+
+def bidi(request):
+    """Adds to the context BiDi related variables
+
+    LANGUAGE_DIRECTION -- Direction of the current language ('ltr' or 'rtl')
+    LANGUAGE_START -- Start of the language layout ('right' for rtl, 'left'
+                      for 'ltr')
+    LANGUAGE_END -- End of the language layout ('left' for rtl, 'right'
+                      for 'ltr')
+
+    """
+
+    from django.utils import translation
+
+    if translation.get_language_bidi():
+        extra_context = {
+            'LANGUAGE_DIRECTION':'rtl',
+            'LANGUAGE_START':'right',
+            'LANGUAGE_END':'left',
+        }
+    else:
+        extra_context = {
+            'LANGUAGE_DIRECTION':'ltr',
+            'LANGUAGE_START':'left',
+            'LANGUAGE_END':'right',
+        }
+
+    return extra_context
