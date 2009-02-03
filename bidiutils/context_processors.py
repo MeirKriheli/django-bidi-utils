5492b3a52db85989cf97be8cbf796c9d9b654e27
Meir Kriheli <meir@mksoft.co.il>
2009-02-03 02:41:43 +0200

    Now passes LANGUAGE_MARKER tests

--- bidiutils/context_processors.py
+++ bidiutils/context_processors.py
@@ -3,26 +3,31 @@
 def bidi(request):
     """Adds to the context BiDi related variables
 
-    LANGUAGE_DIRECTION -- Direction of the current language ('ltr' or 'rtl')
-    LANGUAGE_START -- Start of the language layout ('right' for rtl, 'left'
+    LANGUAGE_DIRECTION -- Direction of current language ('ltr' or 'rtl')
+    LANGUAGE_START -- Start of language layout ('right' for rtl, 'left'
                       for 'ltr')
-    LANGUAGE_END -- End of the language layout ('left' for rtl, 'right'
+    LANGUAGE_END -- End of language layout ('left' for rtl, 'right'
                       for 'ltr')
+    LANGUAGE_MARKER -- Language marker entity ('&rlm;' for rtl, '&lrm'
+                       for ltr)
 
     """
-
     from django.utils import translation
+    from django.utils.safestring import mark_safe
+
     if translation.get_language_bidi():
         extra_context = {
             'LANGUAGE_DIRECTION':'rtl',
             'LANGUAGE_START':'right',
             'LANGUAGE_END':'left',
+            'LANGUAGE_MARKER': mark_safe('&rlm;'),
         }
     else:
         extra_context = {
             'LANGUAGE_DIRECTION':'ltr',
             'LANGUAGE_START':'left',
             'LANGUAGE_END':'right',
+            'LANGUAGE_MARKER': mark_safe('&lrm;'),
         }
 
     return extra_context
